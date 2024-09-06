163ef92f0750dcfe5f6f6a5e951562b48fd80404
Débora Barroggi Constantino <129330607+deborabconstantino@users.noreply.github.com>
2024-09-06 15:48:32 +0100

    Add files via upload

---
+++ calendar_based_graphics.R
@@ -0,0 +1,58 @@
+rm(list=ls(all=TRUE))
+
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(reshape2)
+library(rstatix)
+library(ez)
+library(ggplot2)
+library(pacman)
+library(ggpubr)
+library(psych)
+library(RVAideMemoire) # to perform shapiro by group
+library(dunn.test)
+library(ggdist)
+library(gghalves)
+library(esquisse)
+library(zoo)
+library(fpp)
+library(circlize)
+library(corrplot)
+library(gt) # nice tables, summary statistics
+library(gtExtras)# data visualisation with tables
+library(ggExtra)
+library(ggside)
+library(gtsummary)
+library(ggdist)
+library(tidyverse)
+library(tidyquant)
+library(ggthemes)
+library(sugrrants)
+
+install.packages("sugrrants")
+
+# reading csv and specifying types of date and time columns
+
+db <- read_csv("C:/Users/dc00955/OneDrive - University of Surrey/Desktop/Sara_case_report/activity_1part_test.csv",
+               col_types = cols(date = col_date("%Y/%m/%d"), col_time("%H/%M/%S")))
+
+
+hourly_peds %>%
+  filter(Date < as.Date("2022-09-22")& Date <= as.Date("2022-12-22")) %>%  # filtering 3 months
+  ggplot(aes(x = Time, y = Hourly_Counts, colour = Sensor_Name)) +
+  geom_line() +
+  facet_calendar(~ Date) + # a variable contains dates
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+p <- hourly_peds %>%
+  filter(Date < as.Date("2022-09-22")& Date <= as.Date("2022-12-22")) %>%
+  mutate(Weekend = if_else(Day %in% c("Saturday", "Sunday"), "Weekend", "Weekday")) %>%
+  frame_calendar(x = Time, y = Hourly_Counts, date = Date) %>% 
+  ggplot(aes(x = .Time, y = .Hourly_Counts, group = Date, colour = Weekend)) +
+  geom_line() +
+  theme(legend.position = "bottom")
+prettify(p)
+
+p
